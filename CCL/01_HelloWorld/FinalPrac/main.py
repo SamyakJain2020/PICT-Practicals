069b35d827dca1a6b124a1c75bed781e36867213
SamyakJain2020 <samyakjainking@gmail.com>
2023-05-29 18:15:21 +0530

    a

--- CCL/01_HelloWorld/FinalPrac/main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write("Final prac done")
-
-app=webapp2.WSGIApplication([("/",MainPage)],debug=True)
-
-# WSGI => WEB SERVER GATEWAY INTERFACE
--- CCL/02_Print_NO_SeatNo_Name/main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers["Content-Type"]="text/plain"
-		for i in range(5):
-			self.response.out.write("Name => Shubham Sukum   Roll No => 33371    Department => Information Technology\n")
-
-app=webapp2.WSGIApplication([("/",MainPage)],debug=True)
